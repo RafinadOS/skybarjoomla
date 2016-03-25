0ba14b4f514c0b6ad898647c9bad8f4665d03ba4
Павел Рафеев < >
2016-03-25 20:27:47 +0200

    index

--- js/main.js
+++ js/main.js
@@ -8,6 +8,32 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+$('.inst-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        500:{
+            items:4
+        },
+        600:{
+            items:5
+        },
+        980:{
+            items:8
+        },
+        1200:{
+            items:6
+        },
+        1600:{
+            items:9
+        }
+    }
+});
+
 
 $(document).ready(function($) {
     $('.overlay').removeClass('overlay-show');
@@ -17,9 +43,47 @@ $(document).ready(function($) {
         $('.overlay').toggleClass('overlay-show');
         return false;
     });
+    $('.instagramm-scroll__wrapper-show').click(function(e) {
+        $('body').toggleClass('instagramm-scroll-show');
+        $('.overlay').toggleClass('overlay-show');
+        return false;
+    });
+    $('.footer__text-newsletter').click(function(e) {
+        $('#newsletter').toggleClass('md-show');
+        $('.overlay').toggleClass('overlay-show');
+        $('.overlay').toggleClass('overlay-bg');
+        return false;
+    });
+
+     $('.footer__text-callback').click(function(e) {
+        $('#callback').toggleClass('md-show');
+        $('.overlay').toggleClass('overlay-show');
+        $('.overlay').toggleClass('overlay-bg');
+        return false;
+    });
+
+    $('.md-content__close').click(function(e) {
+        $('#newsletter').removeClass('md-show');
+        $('#callback').removeClass('md-show');
+        $('.overlay').removeClass('overlay-show');
+        $('.overlay').removeClass('overlay-bg');
+        return false;
+    });
+
+    $('.header-menu__open').click(function(e){
+        $('body').toggleClass('header-menu__open-hide');
+        $('.overlay').toggleClass('overlay-show');
+        return false;
+    });
+
     $('.overlay').click(function() {
         $(this).removeClass('overlay-show');
-        $('body').toggleClass('show-shared')
+        $(this).removeClass('overlay-bg');
+        $('body').removeClass('show-shared');
+        $('body').removeClass('instagramm-scroll-show');
+        $('body').removeClass('header-menu__open-hide');
+        $('#newsletter').removeClass('md-show');
+        $('#callback').removeClass('md-show');
     });
 
 });
